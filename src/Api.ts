e722196703713a544fd545c275e8766afbc3ec75
Thiago Borges Mansano <thiagobmansano@gmail.com>
2024-04-17 22:08:11 -0300

    fix: ajustar .env do projeto

--- src/Api.ts
+++ src/Api.ts
@@ -1,76 +1,60 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API = import.meta.env.VITE_API_TESTE
-console.log(API)
+const API_BASE_URL = import.meta.env.VITE_API_TESTE || process.env.API_URL;
 
 const urlSincro = axios.create({
-  baseURL: API,
-})
+  baseURL: API_BASE_URL,
+});
 
 function setToken(token: string) {
   const config = {
     headers: {
-      'Authorization': 'Bearer ' + token
-    }
-  }
-  return config
+      Authorization: 'Bearer ' + token,
+    },
+  };
+  return config;
 }
-
 
 export async function REGISTER_POST(body: {
-  username: string,
-  password: string,
-  email: string,
-  name: string
+  username: string;
+  password: string;
+  email: string;
+  name: string;
 }) {
-
-  const response = await urlSincro.post('/register', body,)
-  return response
+  const response = await urlSincro.post('/register', body);
+  return response;
 }
 
-
-export async function LOGIN_POST(body: {
-  username: string,
-  password: string,
-}) {
-  const response = await urlSincro.post('/login', body)
-  return response
+export async function LOGIN_POST(body: { username: string; password: string }) {
+  const response = await urlSincro.post('/login', body);
+  return response;
 }
 
 export async function GET_USER(token: string) {
-
-  const config = setToken(token)
-  const response = await urlSincro.get('/user', config)
-  return response
-
+  const config = setToken(token);
+  const response = await urlSincro.get('/user', config);
+  return response;
 }
 
 export async function GET_USER_POKEMONS(token: string) {
-
-  const config = setToken(token)
-  const response = await urlSincro.get('/user/favPokemon', config)
-  return response
-
+  const config = setToken(token);
+  const response = await urlSincro.get('/user/favPokemon', config);
+  return response;
 }
 
 export async function GET_ALL_POKEMONS() {
-
-  const response = (await urlSincro.get('/pokemons')).data.data
-  return response
-
+  const response = (await urlSincro.get('/pokemons')).data.data;
+  return response;
 }
 
 export async function POST_USER_ADD_POKEMONS(token: string, id: string) {
-  const config = setToken(token)
-  const response = await urlSincro.post(`/user/favPokemon/${id}`, {}, config)
-  return response
+  const config = setToken(token);
+  const response = await urlSincro.post(`/user/favPokemon/${id}`, {}, config);
+  return response;
 }
 
-
 export async function DELETE_USER_POKEMONS(token: string, id: string) {
-
-  const config = setToken(token)
-  const response = await urlSincro.delete(`/user/favPokemon/${id}`, config)
-  return response
-
+  const config = setToken(token);
+  const response = await urlSincro.delete(`/user/favPokemon/${id}`, config);
+  return response;
 }
